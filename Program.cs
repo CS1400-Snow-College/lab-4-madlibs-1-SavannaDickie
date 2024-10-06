99f87d02a7df532bf968f3dc9bbcd04c0c799b55
Savanna Dickie <personal3@Savannas-MacBook-Pro.local>
2024-10-06 14:56:48 -0600

    Greeting

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
 //name: Savanna Dickie Date: 10/06/2024 lab: Lab 4 Madlibs #1
+Console.Title = "MADLIBS";
+Console.WriteLine("This is Mad Libs. When you are done providing the requested words \nI will buildthem into a funny sotry for you\nEnter the requested word types.");
+
